25fcc18cd0c91d5ab3b54d9cd0f3353cde4cefc3
ezliu <evanliu@stanford.edu>
2018-05-01 01:29:47 -0700

    Yen's KSP

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
+import copy
 import numpy as np
+from collections import deque
 
 
 class Graph(object):
@@ -17,7 +19,7 @@ class Graph(object):
         def unconnected_vertices(vertices):
             candidates = []
             for i in xrange(len(vertices)):
-                for j in xrange(i, len(vertices)):
+                for j in xrange(i + 1, len(vertices)):
                     if vertices[i].degree < vertex_degree and \
                             vertices[j].degree < vertex_degree and \
                             vertices[i] not in vertices[j].neighbors:
@@ -74,25 +76,95 @@ class Graph(object):
         Args:
             vertices (list[Vertex])
         """
-        self._vertices = vertices
+        self._vertices = dict((v.uid, v) for v in vertices)
 
     def k_shortest_paths(self, k, start, end):
         """Returns the k shortest paths between start and end.
 
         Args:
             k (int)
-            start (int): index of start Vertex
-            end (int): index of end Vertex
+            start (int): uid of start Vertex
+            end (int): uid of end Vertex
 
         Returns:
             list[list[Vertex]]
         """
-        pass
+        def bfs(start, end):
+            visited = set([start])
+            bfs_queue = deque([[start]])
+
+            while len(bfs_queue) > 0:
+                path_prefix = bfs_queue.popleft()
+                last_vertex = path_prefix[-1]
+                if last_vertex == end:
+                    return path_prefix
+
+                for neighbor in last_vertex.neighbors:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        path_copy = copy.copy(path_prefix)
+                        path_copy.append(neighbor)
+                        bfs_queue.append(path_copy)
+
+            return None
+
+        if k == 1:
+            return bfs(self._vertices[start], self._vertices[end])
+
+        # Yen's shortest path algorithm, pseudocode from Wikipedia
+        A = [bfs(self._vertices[start], self._vertices[end])]
+        B = []
+        for _ in xrange(k - 1):
+            for i in xrange(len(A[-1]) - 1):
+                spur_node = A[-1][i]
+                root_path = A[-1][:i + 1]
+
+                edges_removed = []
+                vertices_removed = []
+                for path in A:
+                    if len(path) > i + 1 and root_path == path[:i + 1]:
+                        edges_removed.append((path[i], path[i + 1]))
+                        path[i].remove_neighbor(path[i + 1])
+                        path[i + 1].remove_neighbor(path[i])
+
+                for vertex in root_path[:-1]:
+                    vertices_removed.append(vertex)
+                    self._remove_vertex(vertex)
+
+                spur_path = bfs(spur_node, self._vertices[end])
+                if spur_path is not None:
+                    total_path = root_path[:-1] + spur_path
+                    for path in B:
+                        if tuple(total_path) == tuple(path):
+                            break
+                    else:
+                        B.append(total_path)
+
+                for vertex in vertices_removed:
+                    self._vertices[vertex.uid] = vertex
+                    for neighbor in vertex.neighbors:
+                        neighbor.add_neighbor(vertex)
+
+                for edge in edges_removed:
+                    edge[0].add_neighbor(edge[1])
+                    edge[1].add_neighbor(edge[0])
+
+            if len(B) == 0:
+                break
+            B.sort(key=lambda x: len(x))
+            A.append(B[0])
+            B.pop(0)
+        return A
+
+    def _remove_vertex(self, vertex):
+        for neighbor in vertex.neighbors:
+            neighbor.remove_neighbor(vertex)
+        del self._vertices[vertex.uid]
 
     def __str__(self):
         s = ""
-        for v in self._vertices:
-            s += "{}\n".format(v)
+        for v in self._vertices.itervalues():
+            s += "{} --> {}\n".format(v, sorted(v.neighbors))
         return s
     __repr__ = __str__
 
@@ -110,6 +182,10 @@ class Vertex(object):
         self._neighbors.add(neighbor)
 
     def remove_neighbor(self, neighbor):
+        if neighbor not in self.neighbors:
+            print "Neighbor {} not found when removing from {}".format(
+                    neighbor, self)
+            return
         self._neighbors.remove(neighbor)
 
     @property
@@ -121,8 +197,11 @@ class Vertex(object):
         return len(self._neighbors)
 
     def __str__(self):
-        return "[{}] --> {}".format(self.uid, [n.uid for n in self.neighbors])
+        return "Vertex({})".format(self.uid)
     __repr__ = __str__
 
 
-print Graph.rrg(11, 4)
+graph = Graph.rrg(11, 4)
+print graph
+for path in graph.k_shortest_paths(7, 2, 5):
+    print path
