1a406ccef1970e832c3992f0055f5607c7b29a54
ezliu <evanliu@stanford.edu>
2018-05-09 11:36:53 +0000

    Getting rid of print statement

--- jelly_router.py
+++ jelly_router.py
@@ -11,13 +11,11 @@ class JellyRouting(Routing):
         self._paths = defaultdict(dict)
 
     def get_route(self, src, dst, pkt):
-        print "Get {} --> {}".format(src, dst)
         if src == dst:
             return [src]
         src = self._topo.id_gen(name=src).dpid
         dst = self._topo.id_gen(name=dst).dpid
         if src not in self._paths or dst not in self._paths[src]:
-            print "Not cached"
             graph = self._topo._graph
             #paths = graph.k_shortest_paths(1, src, dst)
             #paths = [self._topo.id_gen(dpid=x.uid).name_str() for path in paths for x in path]
@@ -28,8 +26,5 @@ class JellyRouting(Routing):
             paths = [
                 [self._topo.id_gen(dpid=x.uid).name_str() for x in path] for path in paths]
             self._paths[src][dst] = paths
-        else:
-            print "Cached"
-        print "Self._paths: {}".format(self._paths)
         return self._paths[src][dst][
                 np.random.randint(len(self._paths[src][dst]))]
